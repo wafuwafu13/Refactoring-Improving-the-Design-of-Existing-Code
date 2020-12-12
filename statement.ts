a2fd44619ea846bfa127e68c65d8efb26fb64c5a
wafuwafu13 <mariobaske@i.softbank.jp>
2020-12-12 16:55:04 +0900

    型

--- statement.ts
+++ statement.ts
@@ -18,11 +18,11 @@ function renderPlainText(data: StatementData): string {
   return result;
 }
 
-function htmlStatement(invoice: Invoice) {
+function htmlStatement(invoice: Invoice): string {
   return renderHtml(createStatementData(invoice));
 }
 
-function renderHtml(data: StatementData) {
+function renderHtml(data: StatementData): string {
   let result = `<h1>Statement for ${data.customer}</h1>\n`;
   result += "<table>\n";
   result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
