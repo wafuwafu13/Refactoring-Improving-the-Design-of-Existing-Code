70c483c2e5e50c415ace9739163470fd356b53c5
wafuwafu13 <mariobaske@i.softbank.jp>
2020-12-12 11:05:04 +0900

    p18

--- statement.ts
+++ statement.ts
@@ -12,12 +12,14 @@ function statement(invoice: Invoice): string {
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
     // 注文の内訳を出力
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
+  }
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
   }
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
