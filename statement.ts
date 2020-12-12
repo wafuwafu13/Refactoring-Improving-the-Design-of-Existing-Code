c713a388d03d2581b06369bdb45d54bcc814196e
wafuwafu13 <mariobaske@i.softbank.jp>
2020-12-12 11:07:16 +0900

    19

--- statement.ts
+++ statement.ts
@@ -17,10 +17,7 @@ function statement(invoice: Invoice): string {
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
+  let volumeCredits = totalVolumeCredits()
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
@@ -66,6 +63,14 @@ function usd(aNumber: number): string {
     currency: "USD",
     minimumIntegerDigits: 2,
   }).format(aNumber / 100);
+}
+
+function totalVolumeCredits() {
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+  return volumeCredits
 }
 
 console.log(statement(invoice));
