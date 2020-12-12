c5e87d039301245208997315e83598fa0a025731
wafuwafu13 <mariobaske@i.softbank.jp>
2020-12-12 16:53:21 +0900

    31

--- statement.ts
+++ statement.ts
@@ -16,14 +16,32 @@ function renderPlainText(data: StatementData): string {
   result += `Amount owed is ${usd(data.totalAmount!)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
+}
 
-  function usd(aNumber: number): string {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumIntegerDigits: 2,
-    }).format(aNumber / 100);
+function htmlStatement(invoice: Invoice) {
+  return renderHtml(createStatementData(invoice));
+}
+
+function renderHtml(data: StatementData) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+  for (let perf of data.performances!) {
+    result += ` <tr><td>${perf.play!.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount!)}</td></tr>\n`;
   }
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount!)}</em></p>\n`;
+  return result;
+}
+
+function usd(aNumber: number): string {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumIntegerDigits: 2,
+  }).format(aNumber / 100);
 }
 
 console.log(statement(invoice));
+console.log(htmlStatement(invoice));
