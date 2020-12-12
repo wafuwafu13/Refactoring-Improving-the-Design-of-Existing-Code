9e11ae97d2af7fb139d9960a82a1cb51c40a8bf4
wafuwafu13 <mariobaske@i.softbank.jp>
2020-12-12 09:54:54 +0900

    p8

--- statement.ts
+++ statement.ts
@@ -2,9 +2,12 @@ import plays from "./plays.json";
 import invoice from "./invoices.json";
 
 type Invoice = typeof invoice;
-type Play = typeof plays;
+type Plays = typeof plays;
 
-function statement(invoice: Invoice, plays: Play): string {
+type Performance = typeof invoice.performances[0]
+type Play = Plays[Performance['playID']]
+
+function statement(invoice: Invoice, plays: Plays): string {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
@@ -16,25 +19,7 @@ function statement(invoice: Invoice, plays: Play): string {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    let thisAmount = amountFor(perf, play);
 
     // ボリューム得点のポイントを加算
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -49,6 +34,28 @@ function statement(invoice: Invoice, plays: Play): string {
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+}
+
+function amountFor(perf: Performance, play: Play): number {
+  let thisAmount = 0;
+  switch (play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+  return thisAmount;
 }
 
 console.log(statement(invoice, plays));
