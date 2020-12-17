89dda6c99e80d98280ecb9d37587e72055662a7f
wafuwafu13 <mariobaske@i.softbank.jp>
2020-12-17 10:23:50 +0900

    98

--- src/sampleApplication.test.ts
+++ src/sampleApplication.test.ts
@@ -5,4 +5,8 @@ describe("province", function () {
     const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).toBe(5);
   });
+  it("profit", function () {
+    const asia = new Province(sampleProvinceData());
+    expect(asia.profit).toBe(230);
+  });
 });
