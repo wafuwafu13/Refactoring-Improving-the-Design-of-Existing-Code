7f113d377ef4aee510fd820cfcf9e6096ea19f02
wafuwafu13 <mariobaske@i.softbank.jp>
2020-12-17 10:50:13 +0900

    103

--- src/sampleApplication.test.ts
+++ src/sampleApplication.test.ts
@@ -51,3 +51,16 @@ describe("no producers", function () {
     expect(noProducers.profit).toBe(0);
   });
 });
+
+describe("string for producers", function () {
+  it("", function () {
+    const data = {
+      name: "String producers",
+      producders: "",
+      demand: 30,
+      price: 20,
+    };
+    const prov = new Province(data);
+    expect(prov.shortfall).toBe(0);
+  });
+});
